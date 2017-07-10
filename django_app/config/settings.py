7416b886dec346478de3e29cbe8746809d4e3ad7
ym4199 <ym4199@gmail.com>
2017-07-10 14:22:42 +0900

    tutorial 1 complete

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'snippets',
     'rest_framework',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
