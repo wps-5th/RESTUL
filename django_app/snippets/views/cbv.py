a8915d6ef2d16550b8cc0073e7f8a28ae1cbe2ff
ym4199 <ym4199@gmail.com>
2017-07-10 15:56:29 +0900

    tutorial3 complete

--- django_app/snippets/views/cbv.py
+++ django_app/snippets/views/cbv.py
@@ -1,9 +1,10 @@
 from django.http import Http404
-from rest_framework import status, mixins, generics
+from rest_framework import status, mixins, generics, permissions
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from snippets.models import Snippet
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer
 
 
@@ -88,6 +89,10 @@ class SnippetDetail2(mixins.RetrieveModelMixin,
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    )
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -96,3 +101,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    )
